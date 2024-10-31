5b39e9a7034cf992626dc1a22dea642bdf229f18
Tuan <luyenbigdata@gmail.com>
2024-10-31 22:02:21 +0700

    Change database password

--- convertdl.py
+++ convertdl.py
@@ -26,12 +26,13 @@ for x in range(1000001):
     dataconvert[18] = float(temp[18])
     dataconvert[19] = float(temp[19])
     dulieu.append(dataconvert)
-    print(dataconvert)
+    # print(dataconvert)
 
 filedoc.close()
 
+print('Data loaded')
 
-with pgsql.Connection(("localhost", 5432), "postgres", "12345", "postgres", tls = False) as db:
+with pgsql.Connection(("localhost", 5432), "postgres", "24032003", "postgres", tls = False) as db:
      with db.transaction():
         with db.prepare("INSERT INTO dulieudean VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)") as insert_person:
             for person in dulieu:
@@ -41,3 +42,5 @@ with pgsql.Connection(("localhost", 5432), "postgres", "12345", "postgres", tls 
     # with db.prepare("SELECT count (*) as dem FROM dulieudean") as people:
     #     for person in people():
     #         print(person)
+    
+print('Done')
--- djangodean/appdean/views.py
+++ djangodean/appdean/views.py
@@ -7,7 +7,7 @@ import pgsql
 def listview(request):
     template = loader.get_template('viewdata.html')
     datas = []
-    with pgsql.Connection(("localhost", 5432), "postgres", "12345", "postgres", tls = False) as db:
+    with pgsql.Connection(("localhost", 5432), "postgres", "24032003", "postgres", tls = False) as db:
         statement = db.prepare("SELECT * FROM dulieudean LIMIT 1000")
         datas = list(statement())
         
@@ -21,7 +21,7 @@ def listview(request):
 def detailview(request):
     template = loader.get_template('viewdetail.html')
     A = request.GET.get('q', 'default')
-    with pgsql.Connection(("localhost", 5432), "postgres", "12345", "postgres", tls = False) as db:
+    with pgsql.Connection(("localhost", 5432), "postgres", "24032003", "postgres", tls = False) as db:
 
         with db.prepare("SELECT * FROM dulieudean where batch_id = $1") as person:
             detail = person(A).row()
